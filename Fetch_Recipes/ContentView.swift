21bfdae9717488313a6c6493950ce14702e0732b
EricWeideman <weideman00@gmail.com>
2024-10-23 23:59:36 -0500

    Initial Commit

---
+++ Fetch_Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch_Recipes
+//
+//  Created by Eric Weideman on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch_Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch_Recipes
+//
+//  Created by Eric Weideman on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
